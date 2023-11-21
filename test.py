36a46c276ea4b51a50cd0bc9c43f12fcbf5467af
rshaurya24 <shaurya.r.pethe@gmail.com>
2023-11-21 22:11:37 +0530

    Working on multiple tests...

--- test.py
+++ test.py
@@ -1,63 +1,71 @@
-from kivymd.app import MDApp
-from kivymd.uix.boxlayout import MDBoxLayout
-from kivymd.uix.button import MDIconButton
-from kivymd.uix.card import MDCard
-from kivymd.uix.label import MDLabel
-from kivymd.uix.relativelayout import MDRelativeLayout
-from kivymd.uix.screen import MDScreen
-import pandas as pd
+from kivy.lang import Builder
+from kivy.properties import StringProperty
 
-def index_dataframe(rows):
-    d = {}
-    df = pd.read_csv('crypto-dataset/Index/Index.csv', index_col=0, nrows=rows)
-    for i in df['Symbol']:
-        d[i] = "#0ee0e8"
-    return d
+from kivymd.app import MDApp
+from kivymd.uix.card import MDCard
+
+KV = '''
+<MD3Card>
+    padding: 4
+    size_hint: None, None
+    size: "200dp", "100dp"
+
+    MDRelativeLayout:
+		id: relative
+
+        MDIconButton:
+            icon: "symbol_images/btc.png"
+            icon_size: "80dp"
+            pos_hint: {"top": 1, "right": 1}
+            on_press: 
+        
+
+        MDLabel:
+            id: label
+            text: root.text
+            adaptive_size: True
+            color: "grey"
+            pos: "12dp", "12dp"
+            bold: True
+
+
+MDScreen:
+
+    StackLayout:
+        id: box
+        spacing: [50, 30]
+        padding: [50, 200, 0, 0]
+        adaptive_size: True
+'''
+
 
 class MD3Card(MDCard):
     '''Implements a material design v3 card.'''
-    pass
 
+    text = StringProperty()
+
+symbol_dict = ['BTC', 'ETH', 'ADA', 'MATIC', 'DOGE', 'BUSD', 'SOL', 'LTC', 'ATOM', 'MIOTA',
+               'LN', 'LUSD', 'HNT', 'LQTY', 'SYS', 'ANT', 'BCD', 'TROY', 'XPR', 'AUTO',
+               'DMD', 'CRTS', 'OOE', 'QSP', 'BTSE', 'MAN', 'CRPT', 'BASIC', 'NAV',
+               'STAKE', 'CRP', 'PROB', 'SHFT', 'KARMA', 'ATRI', 'SLIM', 'LITH', 'UNB',
+               'ADS', 'BTS', 'EUROC', 'ETN']
 
 class Example(MDApp):
     def build(self):
         self.theme_cls.theme_style = "Dark"
-        return (
-            MDScreen(
-                MDBoxLayout(
-                    id="box",
-                    adaptive_size=True,
-                    spacing="56dp",
-                    pos_hint={"center_x": 0.5, "center_y": 0.5},
-                )
-            )
-        )
+        return Builder.load_string(KV)
 
     def on_start(self):
-        self.root.ids.box.add_widget(
-			MD3Card(
-				MDRelativeLayout(
-					MDIconButton(
-						icon="images/logo.png",
-						icon_size="50dp",
-						pos_hint={"top": 1, "right": 1}
-					),
-					MDLabel(
-						text="first".capitalize(),
-						# adaptive_size=True,
-						color="#0ee0e8",
-						pos=("12dp", "12dp"),
-					),
-				),
-				line_color=(0.2, 0.2, 0.2, 0.8),
-				style="elevated",
-				padding="4dp",
-				size_hint=(None, None),
-				size=("250dp", "100dp"),
-				md_bg_color="##171717",
-				shadow_softness=2 ,
-				shadow_offset=(0, 1),
-			)
-		)
+        for i in symbol_dict:
+            self.root.ids.box.add_widget(
+                MD3Card(
+                    line_color=(0.2, 0.2, 0.2, 0.8),
+                    style="elevated",
+                    text=i,
+                    md_bg_color="##171717",
+                    shadow_offset=(0, -1),
+                )
+            )
+
 
 Example().run()
