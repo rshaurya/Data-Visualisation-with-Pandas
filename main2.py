6ccb7dbeead7362d6f70a66944cc7948e82e60db
rshaurya24 <shaurya.r.pethe@gmail.com>
2023-11-28 17:09:28 +0530

    Made some changes...

--- main2.py
+++ main2.py
@@ -1,12 +1,12 @@
 from kivymd.app import MDApp
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
+from kivy.uix.image import Image
+from kivy.uix.stacklayout import StackLayout
 
-symbol_dict = ['BTC', 'ETH', 'ADA', 'MATIC', 'DOGE', 'BUSD', 'SOL', 'LTC', 'ATOM', 'MIOTA',
-               'LN', 'LUSD', 'HNT', 'LQTY', 'SYS', 'ANT', 'BCD', 'TROY', 'XPR', 'AUTO',
-               'DMD', 'CRTS', 'OOE', 'QSP', 'BTSE', 'MAN', 'CRPT', 'BASIC', 'NAV',
-               'STAKE', 'CRP', 'PROB', 'SHFT', 'KARMA', 'ATRI', 'SLIM', 'LITH', 'UNB',
-               'ADS', 'BTS', 'EUROC', 'ETN']
+import pandas as pd
+import matplotlib.pyplot as plt
+
 
 
 class HomeScreen(Screen):
@@ -18,9 +18,12 @@ class OverviewScreen(Screen):
 
 class MarketScreen(Screen):
     def on_enter(self, *args):
-        from card import Example, MD3Card
+        from card import Market, MD3Card
+            
+class HistoryScreen(Screen):
+    pass
         
-    
+
 class MainApp(MDApp):
     def build(self):
         Builder.load_file("main2.kv")
@@ -30,6 +33,7 @@ class MainApp(MDApp):
         sm.add_widget(HomeScreen(name="home_screen"))
         sm.add_widget(OverviewScreen(name="overview_screen"))
         sm.add_widget(MarketScreen(name="market_screen"))
+        sm.add_widget(HistoryScreen(name="history_screen"))
 
         self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_hue = "200"
