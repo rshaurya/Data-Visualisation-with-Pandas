f3fd08d13dc43c095ec71aedfbba21df72722d38
rshaurya24 <shaurya.r.pethe@gmail.com>
2023-11-28 14:20:19 +0530

    Added market screen.

--- main2.py
+++ main2.py
@@ -1,9 +1,6 @@
 from kivymd.app import MDApp
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
-from kivymd.uix.button import MDRectangleFlatButton
-from kivymd.uix.card import MDCard
-from kivy.properties import StringProperty
 
 symbol_dict = ['BTC', 'ETH', 'ADA', 'MATIC', 'DOGE', 'BUSD', 'SOL', 'LTC', 'ATOM', 'MIOTA',
                'LN', 'LUSD', 'HNT', 'LQTY', 'SYS', 'ANT', 'BCD', 'TROY', 'XPR', 'AUTO',
@@ -12,34 +9,17 @@ symbol_dict = ['BTC', 'ETH', 'ADA', 'MATIC', 'DOGE', 'BUSD', 'SOL', 'LTC', 'ATOM
                'ADS', 'BTS', 'EUROC', 'ETN']
 
 
-# Function to create dataframe from csv files 
-# by taking 2 inputs: symbol and name of cryptocurrency
-
-
 class HomeScreen(Screen):
     pass
 
 class OverviewScreen(Screen):
     pass
 
-class MD3Card(MDCard):
-    text = StringProperty()
 
-class MarketScreen(Screen, MDCard):
-
-    # text = StringProperty()
-    
-    def Card(self):
-        for i in symbol_dict:
-            self.add_widget(
-                MD3Card(
-                    line_color=(0.2, 0.2, 0.2, 0.8),
-                    style="elevated",
-                    text=i,
-                    md_bg_color="##171717",
-                    shadow_offset=(0, -1),
-                )
-            )
+class MarketScreen(Screen):
+    def on_enter(self, *args):
+        from card import Example, MD3Card
+        
     
 class MainApp(MDApp):
     def build(self):
@@ -54,9 +34,5 @@ class MainApp(MDApp):
         self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_hue = "200"
         return sm
-    
-    
-    
-
 
 MainApp().run()
